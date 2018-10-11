6e2c7ae591b4e24ad3740da6ef061e93de4cbd5b
Leon <jprothwell@gmail.com>
2018-10-11 19:21:37 +0800

    done

---
+++ Example/Pods/Target Support Files/TopViewControllerSwift/TopViewControllerSwift-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TopViewControllerSwift : NSObject
+@end
+@implementation PodsDummy_TopViewControllerSwift
+@end
---
+++ TopViewControllerSwift/Classes/TopViewController.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+func _keyWindow() -> UIWindow?{
+    return UIApplication.shared.keyWindow ?? UIApplication.shared.delegate?.window ?? nil
+}
+
+func _TopVisibleViewController(vc:UIViewController?) -> UIViewController? {
+    if let tabBarController = vc as? UITabBarController {
+        return _TopVisibleViewController(vc: tabBarController.selectedViewController)
+    }
+    else if let navigationController = vc as? UINavigationController {
+        return _TopVisibleViewController(vc: navigationController.visibleViewController)
+    }
+    else if let pv = vc?.presentedViewController {
+        return _TopVisibleViewController(vc: pv)
+    }
+    else if vc?.childViewControllers.isEmpty == false {
+        return _TopVisibleViewController(vc: vc?.childViewControllers.last)
+    }
+    return vc
+}
+
+public func TopViewController()->UIViewController?{
+    return _TopVisibleViewController(vc: RootViewController())
+}
+
+public func RootViewController()->UIViewController?{
+    return _keyWindow()?.rootViewController
+}
